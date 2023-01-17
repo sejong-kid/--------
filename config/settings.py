30aafdbc08bb32307ab235c57fac9c05d61dc29e
qocksgh <123qocksgh@naver.com>
2023-01-17 09:46:27 +0900

    import dj_database_url

--- config/settings.py
+++ config/settings.py
@@ -10,6 +10,15 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
+import dj_database_url
+# 서버 연결 import
+DATABASES = {
+    'default': dj_database_url.config(
+        conn_max_age=600,
+        conn_health_checks=True,
+    ),
+}
+# 서버 연결 
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
