4adc554b997a57feaa563960dd2592e39723b6ba
qocksgh <123qocksgh@naver.com>
2023-01-25 09:39:00 +0900

    세팅 123번째 줄 추가

--- config/settings.py
+++ config/settings.py
@@ -120,8 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 import os
 STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static_files")
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField  "
